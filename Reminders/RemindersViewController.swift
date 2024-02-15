f92ec6a1832e0ca5d72ffdacbc8e33d71090ac97
Badal Aryal <badal.aryal@gmail.com>
2024-02-15 15:11:32 +0545

    Create TableView Delegate Methods to add accessories and action on selection

--- Reminders/RemindersViewController.swift
+++ Reminders/RemindersViewController.swift
@@ -29,5 +29,19 @@ class RemindersViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // print(itemArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
 }
 
