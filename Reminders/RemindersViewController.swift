13ee64869c767a7249b405fadf7059a4f6967fa0
Badal Aryal <badal.aryal@gmail.com>
2024-02-15 14:42:03 +0545

    Subclass a UITableViewController

---
+++ Reminders/RemindersViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Reminders
+//
+//  Created by Badal  Aryal on 15/02/2024.
+//
+
+import UIKit
+
+class RemindersViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+        
+    }
+    
+    //MARK: - TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReminderCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+}
+
