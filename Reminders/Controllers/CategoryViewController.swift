9c2598103b4d03e80d939b54816848d713c5df52
Badal Aryal <badal.aryal@gmail.com>
2024-02-22 13:04:16 +0545

    Add a separate category screen and create a relationship table in Coredata

---
+++ Reminders/Controllers/CategoryViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryViewController.swift
+//  Reminders
+//
+//  Created by Badal  Aryal on 22/02/2024.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+   
+    //MARK: - TableView DataSource Methods
+    
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+    //MARK: - Data Manipulation Methods
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    //MARK: - TableView Delegate Methods
+}
