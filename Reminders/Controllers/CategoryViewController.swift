db9a1f7ea12051d7ed82ea85fa9b6ab1f0c9c86f
Badal Aryal <badal.aryal@gmail.com>
2024-02-27 17:12:25 +0545

    Modularize Code and inherit from SwipeTableViewController to CategoryViewController

--- Reminders/Controllers/CategoryViewController.swift
+++ Reminders/Controllers/CategoryViewController.swift
@@ -6,15 +6,14 @@
 //
 
 import UIKit
-import SwipeCellKit
 import RealmSwift
 
-class CategoryViewController: UITableViewController {
- 
+class CategoryViewController: SwipeTableViewController {
+    
     // initialise a new realm for category
     let realm = try! Realm()
     var categories: Results<Category>?
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,27 +23,22 @@ class CategoryViewController: UITableViewController {
         
         
     }
-   
+    
     //MARK: - TableView DataSource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories?.count ?? 1
     }
     
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! SwipeTableViewCell
-//        cell.delegate = self
-//        return cell
-//    }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         print("cellForRowAtIndexPath at CategoryViewController  Called")
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! SwipeTableViewCell
-     
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added Yet"
-        cell.delegate = self
+        
         return cell
         
     }
@@ -80,8 +74,29 @@ class CategoryViewController: UITableViewController {
     
     func loadCategories() {
         
-         categories = realm.objects(Category.self)
-
+        categories = realm.objects(Category.self)
+        
+        tableView.reloadData()
+    }
+    
+    //MARK: - Delete Data from Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+        
+        
+        
+        if let categoryForDeletion = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(categoryForDeletion)
+                }
+            } catch {
+                print("Error deleting category, \(error)")
+            }
+            
+            
+        }
+        
     }
     
     //MARK: - Add New Categories
@@ -113,42 +128,4 @@ class CategoryViewController: UITableViewController {
     }
     
 }
- 
-//MARK: - Swipe Cell Delegate Methods
-extension CategoryViewController: SwipeTableViewCellDelegate {
-    
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
-        guard orientation == .right else { return nil }
-        
-        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
-            // handle action by updating model with deletion
-            
-            if let categoryForDeletion = self.categories?[indexPath.row] {
-                do {
-                    try self.realm.write {
-                        self.realm.delete(categoryForDeletion)
-                    }
-                } catch {
-                    print("Error deleting category, \(error)")
-                }
-                
-               
-            }
-        }
-            // customize the action appearance
-            deleteAction.image = UIImage(named: "delete-icon")
-            
-            
-            return [deleteAction]
-        }
-    
-    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
-        var options = SwipeOptions()
-        options.expansionStyle = .destructive
-        return options
-    }
-
-        
-        
-    }
 
