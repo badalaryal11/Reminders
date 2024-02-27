8cfbcae1adf2d31ec0896d91884fba356ee5aa8d
Badal Aryal <badal.aryal@gmail.com>
2024-02-27 23:48:06 +0545

    Add colours to our  Category Screen by using  Chameleon Framework pod

--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 # Uncomment the next line to define a global platform for your project
  platform :ios, '13.0'
 
+
 target 'Reminders' do
   
 
@@ -9,6 +10,8 @@ target 'Reminders' do
     pod 'RealmSwift', '~> 10.47.0', :modular_headers => true
     pod 'Realm', '~> 10.47.0', :modular_headers => true
     pod 'SwipeCellKit'
+    pod 'ChameleonFramework/Swift', :git => 'https://github.com/wowansm/Chameleon.git', :branch => 'swift5'
+    
    
 
 end
--- Reminders/Controllers/CategoryViewController.swift
+++ Reminders/Controllers/CategoryViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class CategoryViewController: SwipeTableViewController {
     
@@ -38,6 +39,8 @@ class CategoryViewController: SwipeTableViewController {
         let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added Yet"
+        
+        cell.backgroundColor = UIColor.randomFlat()
         
         return cell
         
